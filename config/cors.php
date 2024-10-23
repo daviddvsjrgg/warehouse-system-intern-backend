7ec489c50360206fce9d238eaa7d508f654183bc
David <daviddwiyanto79@gmail.com>
2024-10-23 10:57:29 +0700

    fix: cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://warehouse-system-intern-frontend.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
